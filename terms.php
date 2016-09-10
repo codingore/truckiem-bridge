01d5614deae3320a8136e53fe7df4c7ea4404d12
Trần Đình Thoại <codingore@gmail.com>
2016-09-10 15:47:07 +0700

    Create terms.php

---
+++ terms.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Terms of Use</title>
+  </head>
+  <body>
+    <div style="white-space: pre-wrap">
+***** Terms of Use *****
+
+1. You must agree not to spam our chatbot
+    </div>
+  </body>
+</html>
