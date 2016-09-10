0fa2897f76c7f2e0ec909f3b908a2f81bff65a7c
Trần Đình Thoại <codingore@gmail.com>
2016-09-10 15:46:41 +0700

    Create privacy.php

---
+++ privacy.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Privacy Policy</title>
+  </head>
+  <body>
+    <div style="white-space: pre-wrap">
+***** Privacy Policy *****
+
+1. INFORMATION WE COLLECT
+
+During chatting, we collect avatar, fullname of users.
+    </div>
+  </body>
+</html>
